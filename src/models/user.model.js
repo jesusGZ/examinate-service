0334556ccba5e83f9af3f86ce7b44c65e26ff527
jesusGZ <j.gr.zurita@gmail.com>
2022-01-07 13:51:09 -0600

    modificacion de modelo

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -6,14 +6,6 @@ const userSchema = new Schema({
 	id: Schema.Types.ObjectId,
 	nombre: { type: String, required: true },
 	email: { type: String, unique: true, required: true },
-	direccion: { type: String, required: false },
-	telefono: [
-		{
-			fijo: { type: String, unique: true, required: false },
-			movil: { type: String, unique: true, required: false },
-		},
-	],
-	cv_uri: { type: String, unique: true, required: false },
 	user: { type: String, unique: true, required: true },
 	password: { type: String, required: true },
 	activo: { type: Boolean, default: true, required: false },
