421f53e10665a2f580c0e86efe07b64f4b7cf312
jesusGZ <j.gr.zurita@gmail.com>
2022-01-07 11:07:34 -0600

    user service

---
+++ src/services/user.service.js
@@ -0,0 +1,109 @@
+const userModel = require('../models/user.model');
+
+module.exports = class userService {
+	async insertUser(data) {
+		const nuevo_usuario = await new userModel(data);
+		await nuevo_usuario.save();
+		return nuevo_usuario;
+	}
+
+	async updateUser(data) {
+		const id = data.id;
+		delete data.id;
+
+		const result = await userModel.updateOne({ _id: id }, data);
+		return result;
+	}
+
+	async updateUserPassword(user, password) {
+		const result = await userModel.updateOne({ _id: user._id }, { password: password });
+		return result;
+	}
+
+	async getEmail(email) {
+		let data = await userModel.findOne({ email: email });
+		if (data) data = data.toObject();
+		return data;
+	}
+
+	async getEmailDistincId(email, id) {
+		let data = await userModel.findOne({ email: email, _id: { $ne: id } });
+		if (data) data = data.toObject();
+		return data;
+	}
+
+	async getTelefonoMovilDistincId(telefono_movil, id) {
+		let data = await userModel.findOne({ 'telefono.movil': telefono_movil, _id: { $ne: id } });
+		if (data) data = data.toObject();
+		return data;
+	}
+
+	async getTelefonoFijoDistincId(telefono_fijo, id) {
+		let data = await userModel.findOne({ 'telefono.fijo': telefono_fijo, _id: { $ne: id } });
+		if (data) data = data.toObject();
+		return data;
+	}
+
+	async getCvUriDistincId(cv_uri, id) {
+		let data = await userModel.findOne({ cv_uri: cv_uri, _id: { $ne: id } });
+		if (data) data = data.toObject();
+		return data;
+	}
+
+	async getUser(user) {
+		let data = await userModel.findOne({ user: user });
+		if (data) data = data.toObject();
+		return data;
+	}
+
+	async getUserById(id) {
+		let data = await userModel.findOne({ _id: id });
+		if (data) data = data.toObject();
+		return data;
+	}
+
+	async getAllUsers() {
+		let data = await userModel.find();
+		return data;
+	}
+
+	async getUserDistincId(user, id) {
+		let data = await userModel.findOne({ user: user, _id: { $ne: id } });
+		if (data) data = data.toObject();
+		return data;
+	}
+
+	async getNameDistincId(name, id) {
+		let data = await userModel.findOne({ nombre: name, _id: { $ne: id } });
+		if (data) data = data.toObject();
+		return data;
+	}
+
+	async getAll() {
+		const users = await userModel.aggregate([{ $project: { password: 0 } }]);
+		return users;
+	}
+
+	async getCredentials(user, password) {
+		const data = await userModel.find({ user: user, password: password });
+		return data;
+	}
+
+	async getPasswordAuth(id) {
+		let data = await userModel.findOne({ _id: id });
+		if (data) data = data.toObject();
+		return data;
+	}
+
+	async getDataKey(id) {
+		let data = await userModel.findOne({ _id: id });
+		if (data) data = data.toObject();
+		return data;
+	}
+
+	async getPasswordById(id) {
+		let data = await userModel.findOne({ _id: id }, 'password');
+		if (data) data = data.toObject();
+		return data;
+	}
+};
