f2fce976280e0bc6490f19768e3deae753ef4d1b
jesusGZ <j.gr.zurita@gmail.com>
2022-01-10 15:53:40 -0600

    authenticateJWToken

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,13 +1,20 @@
 const jwt = require('jsonwebtoken');
 
 const { SECURITY } = require('../core/config');
+const USER_SERVICE = require('../services/user.service');
 
-function authenticateJWToken(req, res, next) {
-	const access_header = req.headers['authorization'];
+async function authenticateJWToken(req, res, next) {
+	const user_service = new USER_SERVICE();
+
+	const access_header = req.headers.authorization;
 	const token = access_header && access_header.split(' ')[1];
 	if (token === null) return res.json({ status: 'error', message: 'No token' });
 
-	jwt.verify(token, SECURITY.JWT_SECRET_KEY, (err, payload) => {
+	const decoded = jwt.decode(token, { complete: false });
+
+	const data_key = await user_service.getPasswordById(decoded.payload);
+
+	jwt.verify(token, SECURITY.JWT_KEY + data_key.password, (err, payload) => {
 		if (err) {
 			res.json({ status: 'error', message: 'Invalid token' });
 		} else {
