b12e6bd2ab798265d055ed24808ab0dccfed5d1f
jesusGZ <j.gr.zurita@gmail.com>
2022-01-11 10:05:56 -0600

    ruta para crear examen

---
+++ src/routes/exams.routes.js
@@ -0,0 +1,38 @@
+const moment = require('moment');
+const mailjet = require('node-mailjet').connect(process.env.MAILJET_API_KEY, process.env.MAILJET_API_SECRET);
+
+const exam_schema = require('../core/validators/schemas/exam.schema');
+const validateRequestMiddleware = require('../core/middlewares/validateRequest');
+const EXAM_CONTROLLER = require('../controllers/exam.controller');
+const authJWT = require('../utils/auth');
+
+var ObjectId = require('mongodb').ObjectId;
+
+const exam_controller = new EXAM_CONTROLLER();
+
+module.exports = (app) => {
+	// ========= CRUD for exams ============
+
+	// --------- CREATE ----------
+
+	// request format to add a exam in the list of all exams:
+	// req.body = {
+	//     newExam: {
+	//         examName: String,
+	//         startDateTime: Date,
+	//         endDateTime: Date,
+	//         questionBankId: String,
+	//         classId: String,
+	//     }
+	// }
+
+	app.post('/exam', authJWT, validateRequestMiddleware(exam_schema.exam, 'body'), async (req, res, next) => {
+		try {
+			const { examName, startDateTime, endDateTime, questionBankId, classId } = req.body.newExam;
+			const result = await exam_controller.createExam({ examName, startDateTime, endDateTime, questionBankId, classId });
+			res.send(result);
+		} catch (error) {
+			next(error);
+		}
+	});
+};
