983a514f33e1f9eac15bd2eb5cd6f4eb148b28c6
jesusGZ <j.gr.zurita@gmail.com>
2022-01-27 13:46:48 -0600

    tags swagger

---
+++ Docs/tags.js
@@ -0,0 +1,24 @@
+module.exports = {
+	tags: [
+		{
+			name: 'user',
+			description: 'Documentación para el uso de las APIs en módulo de usuario',
+		},
+		{
+			name: 'classes',
+			description: 'Documentación para el uso de las APIs en módulo de clases',
+		},
+		{
+			name: 'examlive',
+			description: 'Documentación para el uso de las APIs en módulo de examen en vivo',
+		},
+		{
+			name: 'exams',
+			description: 'Documentación para el uso de las APIs en módulo de examenes',
+		},
+		{
+			name: 'questionBanks',
+			description: 'Documentación para el uso de las APIs en módulo banco de preguntas',
+		},
+	],
+};
