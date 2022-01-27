7bdb80b0a26acb7021e15b058a809e07181c6b7b
jesusGZ <j.gr.zurita@gmail.com>
2022-01-27 13:46:16 -0600

    info swagger

---
+++ Docs/info.js
@@ -0,0 +1,11 @@
+module.exports = {
+	openapi: '3.0.1',
+	info: {
+		title: 'Examinate service',
+		description: "Documentación correspondiente a las API's del servicio de examenes.",
+		contact: {
+			email: '',
+		},
+		version: '1.0.0',
+	},
+};
