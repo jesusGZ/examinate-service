ce35d4b88c1cbb1c38ed8b6249d610007c3fc2f5
jesusGZ <j.gr.zurita@gmail.com>
2022-02-09 14:51:27 -0600

    acomodo de codigo

--- src/core/validators/custom.js
+++ src/core/validators/custom.js
@@ -1,52 +1,67 @@
+const { spaces_regex } = require('../config/regex');
 const { invalid_chars_regex, SQL_keywords_regex, JWT_regex, only_numbers_regex } = require('../config/regex');
+
+const removeSpaces = {
+	validate(value) {
+		return value.replace(spaces_regex, '');
+	},
+};
+
+const removeSpacesInBetween = {
+	validate(value) {
+		const words = value.split(' ');
+		const words_without_spaces = words.filter((word) => word.trim() !== '');
+		return words_without_spaces.join(' ').toString();
+	},
+};
+
+const capitalize = {
+	validate(value) {
+		const words = value.split(' ');
+		const capitalized_words = words.map((word) => word.charAt(0).toUpperCase() + word.slice(1));
+		return capitalized_words.join(' ').toString();
+	},
+};
+
+const hasInvalidChars = {
+	validate(value, helpers) {
+		if (invalid_chars_regex.test(value)) return helpers.error('string.caracteresInvalidos');
+		return value;
+	},
+};
+
+const hasSQLWords = {
+	validate(value, helpers) {
+		if (SQL_keywords_regex.test(value)) return helpers.error('string.caracteresInvalidos');
+		return value;
+	},
+};
+
+const isNotNumber = {
+	validate(value, helpers) {
+		if (only_numbers_regex.test(value)) return helpers.error('string.numeros');
+		return value;
+	},
+};
+
+const jwt = {
+	validate(value, helpers) {
+		if (!JWT_regex.test(value)) return helpers.error('string.jwt');
+		return value;
+	},
+};
 
 const string = (joi) => ({
 	type: 'string',
 	base: joi.string(),
 	rules: {
-		removeSpaces: {
-			validate(value) {
-				return value.replace(/\s/g, '');
-			},
-		},
-		removeSpacesInBetween: {
-			validate(value) {
-				const words = value.split(' ');
-				const words_without_spaces = words.filter((word) => word.trim() !== '');
-				return words_without_spaces.join(' ').toString();
-			},
-		},
-		capitalize: {
-			validate(value) {
-				const words = value.split(' ');
-				const capitalized_words = words.map((word) => word.charAt(0).toUpperCase() + word.slice(1));
-				return capitalized_words.join(' ').toString();
-			},
-		},
-		hasInvalidChars: {
-			validate(value, helpers) {
-				if (invalid_chars_regex.test(value)) return helpers.error('string.caracteresInvalidos');
-				return value;
-			},
-		},
-		hasSQLWords: {
-			validate(value, helpers) {
-				if (SQL_keywords_regex.test(value)) return helpers.error('string.caracteresInvalidos');
-				return value;
-			},
-		},
-		isNotNumber: {
-			validate(value, helpers) {
-				if (only_numbers_regex.test(value)) return helpers.error('string.numeros');
-				return value;
-			},
-		},
-		jwt: {
-			validate(value, helpers) {
-				if (!JWT_regex.test(value)) return helpers.error('string.jwt');
-				return value;
-			},
-		},
+		removeSpaces: removeSpaces,
+		removeSpacesInBetween: removeSpacesInBetween,
+		capitalize: capitalize,
+		hasInvalidChars: hasInvalidChars,
+		hasSQLWords: hasSQLWords,
+		isNotNumber: isNotNumber,
+		jwt: jwt,
 	},
 });
 
