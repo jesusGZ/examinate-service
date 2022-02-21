e5c5b999b0cf6ff989abcbf200697bd7b9c28bf9
jesusGZ <j.gr.zurita@gmail.com>
2022-02-21 14:10:51 -0600

    controlador de rutas

--- app/v1/src/routes/index.routes.js
+++ app/v1/src/routes/index.routes.js
@@ -1,8 +1,17 @@
-const glob = require('glob');
-const path = require('path');
+'use strict';
 
-module.exports = (app) => {
-	glob.sync('./src/routes/*.*.js').forEach((file) => {
-		if (!file.includes('index.routes.js')) require(path.resolve(file))(app);
-	});
-};
+const DefaultRoute = require('./default');
+const UserRoute = require('../modules/user/user.routes');
+const examRoute = require('../modules/exam/exams.routes');
+const examLiveRoute = require('../modules/examLive/examlive.routes');
+const questionBanksRoute = require('../modules/questionBanks/questionBanks.routes');
+
+async function RouteV1(app) {
+	await UserRoute(app);
+	await examRoute(app);
+	await examLiveRoute(app);
+	await questionBanksRoute(app);
+	await DefaultRoute(app);
+}
+
+module.exports = RouteV1;
