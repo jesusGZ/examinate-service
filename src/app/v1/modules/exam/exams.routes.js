0555d78aaf97c3a23066fe5e2748152e4ef052ec
jesusGZ <j.gr.zurita@gmail.com>
2022-02-23 15:53:46 -0600

    manejo de respuestas

--- src/app/v1/modules/exam/exams.routes.js
+++ src/app/v1/modules/exam/exams.routes.js
@@ -1,52 +1,14 @@
-const exam_schema = require('./exam.schema');
 const validateRequestMiddleware = require('../../../../helpers/middleware/validateRequest');
 const exam_controller = require('./exam.controller');
 const authJWT = require('../../../../utils/auth');
+const exam_schema = require('./exam.schema');
 
 module.exports = (router) => {
-	router.post('/exam', authJWT, validateRequestMiddleware(exam_schema.exam, 'body'), async (req, res, next) => {
-		try {
-			const { examName, startDateTime, endDateTime, questionBankId, classId } = req.body;
-			const user = req.payload.user;
+	router.delete('/exam', authJWT, validateRequestMiddleware(exam_schema.examId, 'body'), exam_controller.deleteExam);
 
-			const result = await exam_controller.createExam({ user, examName, startDateTime, endDateTime, questionBankId, classId });
-			res.send(result);
-		} catch (error) {
-			next(error);
-		}
-	});
+	router.post('/exam', authJWT, validateRequestMiddleware(exam_schema.exam, 'body'), exam_controller.createExam);
 
-	router.get('/info', authJWT, async (req, res, next) => {
-		try {
-			const user = req.payload.user;
+	router.get('/exams', authJWT, exam_controller.getExams);
 
-			const result = await exam_controller.getInfo(user);
-			res.send(result);
-		} catch (error) {
-			next(error);
-		}
-	});
-
-	router.get('/exams', authJWT, async (req, res, next) => {
-		try {
-			const user = req.payload.user;
-
-			const result = await exam_controller.getExams(user);
-			res.send(result);
-		} catch (error) {
-			next(error);
-		}
-	});
-
-	router.delete('/exam', authJWT, validateRequestMiddleware(exam_schema.examId, 'body'), async (req, res, next) => {
-		try {
-			const user = req.payload.user;
-			const examId = req.body.examId;
-
-			const result = await exam_controller.deleteExam({ user, examId });
-			res.send(result);
-		} catch (error) {
-			next(error);
-		}
-	});
+	router.get('/info', authJWT, exam_controller.getInfo);
 };
